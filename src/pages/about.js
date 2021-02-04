eded7089d8939a30a6cb84869f1b4f66f854124e
Ryota <onumaryotajh@gmail.com>
2021-02-04 18:52:59 +0900

    first-commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+
+const AboutPage = () => (
+ <Layout>
+ ここはアバウトページ
+ <Link to="/">Go to top</Link> <br />
+ </Layout>
+)
+export default AboutPage
